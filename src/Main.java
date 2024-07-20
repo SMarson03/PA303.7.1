85d98c3fe28f3218416e9d41a0eb0228aff42cca
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-07-19 21:12:46 -0400

    2

--- src/Main.java
+++ src/Main.java
@@ -50,7 +50,14 @@ public class Main {
 //Task 5: Write a program where you create an integer array with a length of 5. Loop through
 // the array and assign the value of the loop control variable (e.g., i) to the corresponding
 // index in the array.
-   
+
+//        not sure I quite understood this task. I found a mild explanation will revisit
+        int i = 1;
+        while (i <= 5){
+            System.out.print(i * 1);
+            i++;
+        }
+
 
 
     }
